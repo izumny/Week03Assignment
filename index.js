48fca8a10ae41fad09dc673cf4c60fe67b20445a
Lilychan2222 <izumny@gmail.com>
2023-04-22 14:52:16 -0700

    second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//Coding Challenge:  As a stretch, you could convert these examples
+// to use prompt() and alert() as was described in the User Input 
+//Section of Week 2: Weekly Videos and Curriculum 
+//(e.g. request the Student name, and the grades).
+
+console.log("Test test test");
